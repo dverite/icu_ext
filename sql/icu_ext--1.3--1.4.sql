bc56556b6cc6d0156a7fa4f6a1d72dcd7c43494a
Daniel Vérité <dverite@gmail.com>
2019-02-15 13:34:34 +0100

    Add icu_replace with collator argument.
    Also rename arguments to match the names of strpos/replace.

--- sql/icu_ext--1.3--1.4.sql
+++ sql/icu_ext--1.3--1.4.sql
@@ -2,24 +2,33 @@
 \echo Use "ALTER EXTENSION icu_ext UPDATE TO '1.4'" to load this file. \quit
 
 CREATE OR REPLACE FUNCTION icu_strpos(
- haystack text,
- needle text
+ string text,
+ "substring" text
 ) RETURNS int4
 AS 'MODULE_PATHNAME', 'icu_strpos'
 LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE COST 10;
 
 CREATE OR REPLACE FUNCTION icu_strpos(
- haystack text,
- needle text,
+ string text,
+ "substring" text,
  collator text
 ) RETURNS int4
 AS 'MODULE_PATHNAME', 'icu_strpos_coll'
 LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE COST 10;
 
 CREATE OR REPLACE FUNCTION icu_replace(
- haystack text,
- oldstr text,
- newstr text
+ string text,
+ "from" text,
+ "to" text
 ) RETURNS text
 AS 'MODULE_PATHNAME', 'icu_replace'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE COST 10;
+LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE COST 100;
+
+CREATE OR REPLACE FUNCTION icu_replace(
+ string text,
+ "from" text,
+ "to" text,
+ collator text
+) RETURNS text
+AS 'MODULE_PATHNAME', 'icu_replace_coll'
+LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE COST 100;
